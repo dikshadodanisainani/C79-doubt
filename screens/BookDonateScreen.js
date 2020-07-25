0a084ef46a512115d6eef75dff6f7a30fbfd971e
dikshadodanisainani <54570385+dikshadodanisainani@users.noreply.github.com>
2020-07-25 23:21:23 +0530

    Add files via upload

---
+++ screens/BookDonateScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View ,TextInput, Alert, TouchableOpacity,Modal, ScrollView, KeyboardAvoidingView} from 'react-native';
+
+export default class BookDonateScreen extends Component
+{
+    render()
+    {
+        return(
+            <View>
+                <Text>
+                    Book Donate screen
+                </Text>
+            </View>
+        )
+    }
+}
